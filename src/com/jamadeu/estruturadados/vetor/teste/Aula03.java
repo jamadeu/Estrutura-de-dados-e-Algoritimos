41f14ed206cea6dc82d4759c0785745e4d5fe276
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-15 06:01:21 -0300

    Aula 03

---
+++ src/com/jamadeu/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,26 @@
+package com.jamadeu.estruturadados.vetor.teste;
+
+import com.jamadeu.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(2);
+		
+//		try {
+//			
+//			vetor.adiciona("elemento 1");
+//			vetor.adiciona("elemento 2");
+//			vetor.adiciona("elemento 3");
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();;
+//		}
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+	}
+
+}
